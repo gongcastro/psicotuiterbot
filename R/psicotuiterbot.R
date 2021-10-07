62be88708bc6cea7d4dbd3b4fc9de6a707c54251
gongcastro <gonzalo.garciadecastro@upf.edu>
2021-10-07 23:46:22 +0200

    First commit

---
+++ R/psicotuiterbot.R
@@ -0,0 +1,16 @@
+# bot R code
+
+
+# Example: post a tweet via the API
+psicotuiterbot_token <- rtweet::create_token(
+    app = "psicotuiterbot",  # the name of the Twitter app
+    consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+    consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+    access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+    access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+rtweet::post_tweet(
+    status = "Tuit de prueba",
+    token = psicotuiterbot_token
+)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# psicotuiter-bot
-Un bot que retuitea tuits que contienen #psicotuiter
+# psicotuiterbot
+
+Un bot que retuitea menciones a #psicotuiter cada 5 minutos. Mantenido por @gongcastro. Para programar este bot he seguido los siguiente tutoriales:
+
+* Para usar GitHub Actions: https://www.rostrum.blog/2020/09/21/londonmapbot/
+* Para dirigir la actividad a un bot asociado: https://dev.to/codebyru/many-bot-accounts-using-1-twitter-developer-account-17ff
