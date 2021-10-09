4cbaaa88b9ba1669bd1594b6c67a1cdd1d632776
gongcastro <gonzalo.garciadecastro@upf.edu>
2021-10-09 19:04:15 +0200

    (Finally) fix Twitter API tokens :)

--- R/bot.R
+++ R/bot.R
@@ -3,7 +3,7 @@
 rtweet::create_token(
     app = "psicotuiterbot",  # the name of the Twitter app
     consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
-    consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+    consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_KEY_SECRET"),
     access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
     access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 )
