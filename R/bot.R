46187607a02b4fe58a7b58a74246c48d962ee5e1
gongcastro <gonzalo.garciadecastro@upf.edu>
2021-10-09 19:44:05 +0200

    Search tweets and RT

--- R/bot.R
+++ R/bot.R
@@ -1,6 +1,12 @@
 # bot R code
 
-rtweet::create_token(
+library(rtweet)
+library(dplyr)
+library(lubridate)
+
+options(httr_oob_default = TRUE)
+
+my_token <- create_token(
     app = "psicotuiterbot",  # the name of the Twitter app
     consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
     consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_KEY_SECRET"),
@@ -8,6 +14,15 @@ rtweet::create_token(
     access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 )
 
-rtweet::post_tweet(
-    status = "Tuit de prueba"
-)
+status_ids <- search_tweets("#psicotuiter", type = "recent", token = my_token) %>% 
+    filter(!is.na(retweet_status_id), created_at > now()-minutes(15)) %>% 
+    pull(status_id)
+
+for (i in 1:length(status_ids)){
+    post_tweet(
+        retweet_id = status_ids[1],
+        token = my_token
+    )
+}
+
+print("Nothing to RT!")
