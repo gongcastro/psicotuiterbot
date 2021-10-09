29833174fa7ada069897bd85e03f9c7616bdfd85
gongcastro <gonzalo.garciadecastro@upf.edu>
2021-10-09 20:05:22 +0200

    Do not load packages

--- R/bot.R
+++ R/bot.R
@@ -1,12 +1,10 @@
 # bot R code
 
-library(rtweet)
 library(dplyr)
-library(lubridate)
 
 options(httr_oob_default = TRUE)
 
-my_token <- create_token(
+my_token <- rtweet::create_token(
     app = "psicotuiterbot",  # the name of the Twitter app
     consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
     consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_KEY_SECRET"),
@@ -14,12 +12,12 @@ my_token <- create_token(
     access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 )
 
-status_ids <- search_tweets("#psicotuiter", type = "recent", token = my_token) %>% 
-    filter(is.na(retweet_status_id), created_at > now(tzone = "UCT")-minutes(15)) %>% 
+status_ids <- rtweet::search_tweets("#psicotuiter", type = "recent", token = my_token) %>% 
+    filter(is.na(retweet_status_id), created_at > lubridate::now(tzone = "UCT")-lubridate::minutes(15)) %>% 
     pull(status_id)
 
 for (i in 1:length(status_ids)){
-    post_tweet(
+    rtweet::post_tweet(
         retweet_id = status_ids[1],
         token = my_token
     )
