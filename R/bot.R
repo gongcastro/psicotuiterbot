496731bc6163dcf923fb8aaa81433d5e835e73b8
gongcastro <gonzalo.garciadecastro@upf.edu>
2021-10-09 22:08:34 +0200

    Comment code

--- R/bot.R
+++ R/bot.R
@@ -1,9 +1,9 @@
 # bot R code
-
 library(dplyr)
 
 options(httr_oob_default = TRUE)
 
+# authenticate Twitter API
 my_token <- rtweet::create_token(
     app = "psicotuiterbot",  # the name of the Twitter app
     consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
@@ -12,10 +12,12 @@ my_token <- rtweet::create_token(
     access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 )
 
+# retrieve mentions to #psicotuiter in the last 15 minutes
 status_ids <- rtweet::search_tweets("#psicotuiter", type = "recent", token = my_token) %>% 
     filter(is.na(retweet_status_id), created_at > lubridate::now(tzone = "UCT")-lubridate::minutes(15)) %>% 
-    pull(status_id)
+    pull(status_id) # get vector with IDs
 
+# RT all IDs
 for (i in 1:length(status_ids)){
     rtweet::post_tweet(
         retweet_id = status_ids[1],
@@ -23,4 +25,3 @@ for (i in 1:length(status_ids)){
     )
 }
 
-print("Nothing to RT!")
