9365e8c141b53214d8ec885ab724574d701e32d4
gongcastro <gonzalo.garciadecastro@upf.edu>
2021-10-17 19:31:51 +0200

    Modifies PR #5 addressing #6

--- R/bot.R
+++ R/bot.R
@@ -14,34 +14,16 @@ my_token <- rtweet::create_token(
 
 # define hashtags
 hashtags <- "#psicotuiter OR #psicotwitter OR #Psicotuiter OR #Psicotwitter OR #PsicoTuiter OR #PsicoTwitter"
+hate_words <- unlist(strsplit(Sys.getenv("HATE_WORDS"), " ")) # words banned from psicotuiterbot (separated by a space)
 time_interval <- lubridate::now(tzone = "UCT")-lubridate::minutes(120)
 
 # retrieve mentions to #psicotuiter in the last 15 minutes
 status_ids <- rtweet::search_tweets(hashtags, type = "recent", token = my_token, include_rts = FALSE) %>% 
     filter(
-        created_at >=  time_interval # 15 min
+        created_at >=  time_interval, # 15 min
+        !grepl(paste(hate_words, collapse = "|"), text) # filter out hate words
     ) %>% 
     pull(status_id) # get vector with IDs
-
-# simple hate words filter
-hate_words <- matrix(c("Gay", "gay", "Maricon", "maricon", "Marica", "marica"), ncol = 1) # words banned from psicotuiterbot (subject to change)
-
-if (length(status_ids) > 0){
-  for (i in length(hate_words)){
-    filt <- paste(hashtags, hate_words[i], sep = " AND ") # adding words to the filter (not sure if needs one AND for each hastag)
-    status_ids_hate <- rtweet::search_tweets(filt, type = "recent", token = my_token, include_rts = FALSE) %>%
-      filter(
-        created_at >=  time_interval # 15 min
-      ) %>% 
-      pull(status_id) # get vector with IDs
-    status_ids <- status_ids %>%
-      filter(
-        !status_ids %in% status_ids_hate[,1] # deleting al the IDs in status_ids_hate from status_ids
-      ) 
-  }
-} else {
-    print("No tweets to filter")
-}
 
 # RT all IDs
 if (length(status_ids) > 0){
