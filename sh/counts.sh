d2c60b1f201ffd1178a6f7c0af186af4c2897c14
gongcastro <gongarciacastro@gmail.com>
2021-11-14 10:50:35 +0100

    Remove points in counts plot

--- sh/counts.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-Rscript -e 'source("R/counts.R")'
-cd ~ 
-find . -name '*rtweet_token*' -delete
-
--- sh/tweet.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-TZ="Spain/Madrid" Rscript -e 'source("R/bot.R")'
-cd ~
-find . -name '*rtweet_token*' -delete
-
