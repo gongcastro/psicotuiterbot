10e1627377e13bb501ac169db54e0d9a203b984a
gongcastro <gongarciacastro@gmail.com>
2022-09-18 16:14:02 +0200

    Save changes

--- R/retweet.R
+++ R/retweet.R
@@ -39,7 +39,7 @@ status_ids <- all_tweets %>%
         created_at >= time_interval, # 15 min
         !grepl(paste(hate_words, collapse = "|"), text), # filter out hate words
         stringr::str_count(text, "#") < 4, # no more than 3 hashtags
-        lang %in% c("es", "und") # in Spanish or undefined language
+        lang %in% c("es", "pt", "und") # in Spanish or undefined language
     ) %>% 
     pull(status_id)
 
